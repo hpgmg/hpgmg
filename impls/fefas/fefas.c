35a67333a0b047c1dcbbad5b51d2a8173e88aada
Jed Brown <jed@jedbrown.org>
2014-03-16 04:49:46 +0100

    fefas: de-unify options parsing

--- impls/fefas/fefas.c
+++ impls/fefas/fefas.c
@@ -12,19 +12,54 @@ struct Options_private {
   PetscReal L[3];
 };
 
-static PetscErrorCode TestGrid(Options opt)
+static PetscErrorCode OptionsParse(const char *header,Options *opt)
 {
   PetscErrorCode ierr;
-  Grid grid;
+  Options o;
+  PetscInt three,M_max;
 
   PetscFunctionBegin;
-  ierr = GridCreate(PETSC_COMM_WORLD,opt->M,opt->p,NULL,opt->cmax,&grid);CHKERRQ(ierr);
-  ierr = GridView(grid);CHKERRQ(ierr);
-  ierr = GridDestroy(&grid);CHKERRQ(ierr);
+  ierr = PetscNew(&o);CHKERRQ(ierr);
+  o->M[0] = 10;
+  o->M[1] = 10;
+  o->M[2] = 10;
+  o->p[0] = 1;
+  o->p[1] = 1;
+  o->p[2] = 1;
+  o->cmax = 100;
+  ierr = PetscOptionsBegin(PETSC_COMM_WORLD,NULL,header,NULL);CHKERRQ(ierr);
+  three = 3;
+  ierr = PetscOptionsIntArray("-M","Fine grid dimensions","",o->M,&three,NULL);CHKERRQ(ierr);
+  three = 3;
+  ierr = PetscOptionsIntArray("-p","Process grid dimensions","",o->p,&three,NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsInt("-cmax","Max coarse grid size","",o->cmax,&o->cmax,NULL);CHKERRQ(ierr);
+  M_max = PetscMax(o->M[0],PetscMax(o->M[1],o->M[2]));
+  o->L[0] = o->M[0]*1./M_max;
+  o->L[1] = o->M[1]*1./M_max;
+  o->L[2] = o->M[2]*1./M_max;
+  three = 3;
+  ierr = PetscOptionsRealArray("-L","Grid dimensions","",o->L,&three,NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsEnd();CHKERRQ(ierr);
+  *opt = o;
   PetscFunctionReturn(0);
 }
 
-static PetscErrorCode TestFESpace(Options opt)
+static PetscErrorCode TestGrid()
+{
+  PetscErrorCode ierr;
+  Grid grid;
+  Options opt;
+
+  PetscFunctionBegin;
+  ierr = OptionsParse("Finite Element FAS Test Grid",&opt);CHKERRQ(ierr);
+  ierr = GridCreate(PETSC_COMM_WORLD,opt->M,opt->p,NULL,opt->cmax,&grid);CHKERRQ(ierr);
+  ierr = GridView(grid);CHKERRQ(ierr);
+  ierr = GridDestroy(&grid);CHKERRQ(ierr);
+  ierr = PetscFree(opt);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+static PetscErrorCode TestFESpace()
 {
   PetscErrorCode ierr;
   Grid grid;
@@ -32,8 +67,10 @@ static PetscErrorCode TestFESpace(Options opt)
   Vec G,L;
   PetscInt i,rstart,rend;
   PetscScalar *g;
+  Options opt;
 
   PetscFunctionBegin;
+  ierr = OptionsParse("Finite Element FAS Test FESpace global-to-local",&opt);CHKERRQ(ierr);
   ierr = GridCreate(PETSC_COMM_WORLD,opt->M,opt->p,NULL,opt->cmax,&grid);CHKERRQ(ierr);
   ierr = DMCreateFESpace(grid,1,1,&dm);CHKERRQ(ierr);
   ierr = GridDestroy(&grid);CHKERRQ(ierr);
@@ -50,10 +87,11 @@ static PetscErrorCode TestFESpace(Options opt)
   ierr = VecDestroy(&G);CHKERRQ(ierr);
   ierr = VecDestroy(&L);CHKERRQ(ierr);
   ierr = DMDestroyFESpace(&dm);CHKERRQ(ierr);
+  ierr = PetscFree(opt);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
 
-static PetscErrorCode TestFEGrad(Options opt)
+static PetscErrorCode TestFEGrad()
 {
   PetscErrorCode ierr;
   Grid grid;
@@ -63,8 +101,10 @@ static PetscErrorCode TestFEGrad(Options opt)
   const PetscReal *B,*D,TestGrad[] = {2,3,5};
   PetscScalar *u,*ue,*du;
   const PetscScalar *l,*x;
+  Options opt;
 
   PetscFunctionBegin;
+  ierr = OptionsParse("Finite Element FAS Test Element Gradients",&opt);CHKERRQ(ierr);
   ierr = GridCreate(PETSC_COMM_WORLD,opt->M,opt->p,NULL,opt->cmax,&grid);CHKERRQ(ierr);
   ierr = DMCreateFESpace(grid,fedegree,1,&dm);CHKERRQ(ierr);
   ierr = GridDestroy(&grid);CHKERRQ(ierr);
@@ -112,10 +152,11 @@ static PetscErrorCode TestFEGrad(Options opt)
   ierr = PetscFree2(ue,du);CHKERRQ(ierr);
   ierr = VecDestroy(&L);CHKERRQ(ierr);
   ierr = DMDestroyFESpace(&dm);CHKERRQ(ierr);
+  ierr = PetscFree(opt);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
 
-static PetscErrorCode ActionParse(int argc,char *argv[],PetscErrorCode (**action)(Options))
+static PetscErrorCode ActionParse(int argc,char *argv[],PetscErrorCode (**action)(void))
 {
   PetscFunctionList actionlist = NULL;
   PetscErrorCode ierr;
@@ -143,42 +184,9 @@ static PetscErrorCode ActionParse(int argc,char *argv[],PetscErrorCode (**action
   PetscFunctionReturn(0);
 }
 
-static PetscErrorCode OptionsParse(Options *opt)
-{
-  PetscErrorCode ierr;
-  Options o;
-  PetscInt three,M_max;
-
-  PetscFunctionBegin;
-  ierr = PetscNew(&o);CHKERRQ(ierr);
-  o->M[0] = 10;
-  o->M[1] = 10;
-  o->M[2] = 10;
-  o->p[0] = 1;
-  o->p[1] = 1;
-  o->p[2] = 1;
-  o->cmax = 100;
-  ierr = PetscOptionsBegin(PETSC_COMM_WORLD,NULL,"Finite Element FAS solver",NULL);CHKERRQ(ierr);
-  three = 3;
-  ierr = PetscOptionsIntArray("-M","Fine grid dimensions","",o->M,&three,NULL);CHKERRQ(ierr);
-  three = 3;
-  ierr = PetscOptionsIntArray("-p","Process grid dimensions","",o->p,&three,NULL);CHKERRQ(ierr);
-  ierr = PetscOptionsInt("-cmax","Max coarse grid size","",o->cmax,&o->cmax,NULL);CHKERRQ(ierr);
-  M_max = PetscMax(o->M[0],PetscMax(o->M[1],o->M[2]));
-  o->L[0] = o->M[0]*1./M_max;
-  o->L[1] = o->M[1]*1./M_max;
-  o->L[2] = o->M[2]*1./M_max;
-  three = 3;
-  ierr = PetscOptionsRealArray("-L","Grid dimensions","",o->L,&three,NULL);CHKERRQ(ierr);
-  ierr = PetscOptionsEnd();CHKERRQ(ierr);
-  *opt = o;
-  PetscFunctionReturn(0);
-}
-
 int main(int argc, char *argv[])
 {
-  PetscErrorCode ierr,(*action)(Options);
-  Options opt;
+  PetscErrorCode ierr,(*action)(void);
 
   PetscInitialize(&argc,&argv,NULL,help);
   ierr = ActionParse(argc,argv,&action);CHKERRQ(ierr);
@@ -186,9 +194,7 @@ int main(int argc, char *argv[])
     PetscFinalize();
     return 1;
   }
-  ierr = OptionsParse(&opt);CHKERRQ(ierr);
-  ierr = (*action)(opt);CHKERRQ(ierr);
-  ierr = PetscFree(opt);CHKERRQ(ierr);
+  ierr = (*action)();CHKERRQ(ierr);
   PetscFinalize();
   return 0;
 }
