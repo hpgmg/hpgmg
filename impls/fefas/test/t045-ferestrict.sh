031f7e84e5f5ad7997eda42283426b06bf76b54b
Jed Brown <jed@jedbrown.org>
2014-03-23 21:26:59 -0600

    fefas test: fix ferestrict test to restrict rather than interpolate

--- impls/fefas/test/t045-ferestrict.sh
+++ impls/fefas/test/t045-ferestrict.sh
@@ -8,8 +8,8 @@ test_expect_stdout 'FE Restriction fedegree=1 serial' 1 'fefas test-ferestrict -
 |u_c - I_h^H u_f|_max =     0
 '
 
-test_expect_stdout 'FE Interpolation fedegree=1 parallel' 4 'fefas test-feinterp -M 6,4,10 -L 6,4,10 -p 2,1,2' '
-|u - I Ihat u|_max =     0
+test_expect_stdout 'FE Restriction fedegree=1 parallel' 4 'fefas test-ferestrict -M 6,4,10 -L 6,4,10 -p 2,1,2' '
+|u_c - I_h^H u_f|_max =     0
 '
 
 test_expect_stdout 'FE Restriction fedegree=1 parallel ragged coarsening' 4 'fefas test-ferestrict -M 4,4,12 -L 1,1,1 -p 1,1,4' '
