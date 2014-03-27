a6fc8dc39aad80f89e17bce7ac5fe913eab025f8
Jed Brown <jed@jedbrown.org>
2014-03-27 14:29:21 -0600

    fefas: add Q2 Poisson and different analytic solutions

--- impls/fefas/op/op-poisson1.c
+++ impls/fefas/op/op-poisson1.c
@@ -1,6 +1,7 @@
 #include "fefas-op.h"
 #include "../tensor.h"
 #include "../pointwise.h"
+#include <strings.h>
 
 #define NE 4
 
@@ -9,21 +10,80 @@ struct Ctx_private {
   int dummy;
 };
 
-static PetscErrorCode OpPointwiseSolution_Poisson1(Op op,const PetscReal x[],const PetscReal L[],PetscScalar u[]) {
+static PetscReal Sqr(PetscReal x) {return x*x;}
+
+static PetscErrorCode OpPointwiseSolution_Poisson_Sine(Op op,const PetscReal x[],const PetscReal L[],PetscScalar u[]) {
   u[0] = PetscSinReal(1*PETSC_PI*x[0]/L[0]) * PetscSinReal(2*PETSC_PI*x[1]/L[1]) * PetscSinReal(3*PETSC_PI*x[2]/L[2]);
   return 0;
 }
-
-static PetscErrorCode OpPointwiseForcing_Poisson1(Op op,const PetscReal x[],const PetscReal L[],PetscScalar f[]) {
-  f[0] = (  PetscSqr(1*PETSC_PI/L[0])*PetscSinReal(1*PETSC_PI*x[0]/L[0]) * PetscSinReal(2*PETSC_PI*x[1]/L[1]) * PetscSinReal(3*PETSC_PI*x[2]/L[2])
-          + PetscSqr(2*PETSC_PI/L[1])*PetscSinReal(1*PETSC_PI*x[0]/L[0]) * PetscSinReal(2*PETSC_PI*x[1]/L[1]) * PetscSinReal(3*PETSC_PI*x[2]/L[2])
-          + PetscSqr(3*PETSC_PI/L[2])*PetscSinReal(1*PETSC_PI*x[0]/L[0]) * PetscSinReal(2*PETSC_PI*x[1]/L[1]) * PetscSinReal(3*PETSC_PI*x[2]/L[2]));
+static PetscErrorCode OpPointwiseForcing_Poisson_Sine(Op op,const PetscReal x[],const PetscReal L[],PetscScalar f[]) {
+  f[0] = (  Sqr(1*PETSC_PI/L[0])*PetscSinReal(1*PETSC_PI*x[0]/L[0]) * PetscSinReal(2*PETSC_PI*x[1]/L[1]) * PetscSinReal(3*PETSC_PI*x[2]/L[2])
+          + Sqr(2*PETSC_PI/L[1])*PetscSinReal(1*PETSC_PI*x[0]/L[0]) * PetscSinReal(2*PETSC_PI*x[1]/L[1]) * PetscSinReal(3*PETSC_PI*x[2]/L[2])
+          + Sqr(3*PETSC_PI/L[2])*PetscSinReal(1*PETSC_PI*x[0]/L[0]) * PetscSinReal(2*PETSC_PI*x[1]/L[1]) * PetscSinReal(3*PETSC_PI*x[2]/L[2]));
   return 0;
 }
 
-static PetscErrorCode OpPointwiseElement_Poisson1(Op op,PetscInt ne,PetscInt Q3,PetscScalar dx[3][3][Q3][NE],PetscReal wdxdet[Q3][NE],PetscScalar du[3][1][Q3][NE],PetscScalar dv[3][1][Q3][NE]) {
+static PetscReal Hump(const PetscReal x[],const PetscReal L[]){
+  return PetscSinReal(PETSC_PI*x[0]/L[0]) * PetscSinReal(PETSC_PI*x[1]/L[1]) * PetscSinReal(PETSC_PI*x[2]/L[2]);}
+static PetscReal Hump_x0(const PetscReal x[],const PetscReal L[]) {
+  return PETSC_PI/L[0]*PetscCosReal(PETSC_PI*x[0]/L[0]) * PetscSinReal(PETSC_PI*x[1]/L[1]) * PetscSinReal(PETSC_PI*x[2]/L[2]);}
+static PetscReal Hump_x1(const PetscReal x[],const PetscReal L[]) {
+  return PETSC_PI/L[1]*PetscSinReal(PETSC_PI*x[0]/L[0]) * PetscCosReal(PETSC_PI*x[1]/L[1]) * PetscSinReal(PETSC_PI*x[2]/L[2]);}
+static PetscReal Hump_x2(const PetscReal x[],const PetscReal L[]) {
+  return PETSC_PI/L[2]*PetscSinReal(PETSC_PI*x[0]/L[0]) * PetscSinReal(PETSC_PI*x[1]/L[1]) * PetscCosReal(PETSC_PI*x[2]/L[2]);}
+static PetscReal Hump_xx0(const PetscReal x[],const PetscReal L[]) {
+  return -Sqr(PETSC_PI/L[0])*Hump(x,L);}
+static PetscReal Hump_xx1(const PetscReal x[],const PetscReal L[]) {
+  return -Sqr(PETSC_PI/L[1])*Hump(x,L);}
+static PetscReal Hump_xx2(const PetscReal x[],const PetscReal L[]) {
+  return -Sqr(PETSC_PI/L[2])*Hump(x,L);}
+
+static PetscReal Bend(const PetscReal x[],const PetscReal L[]) {
+  return PetscTanhReal(x[0]/L[0]) + PetscLogReal(1 + x[1]/L[1]) + PetscExpReal(-x[2]/L[2]);}
+static PetscReal Bend_x0(const PetscReal x[],const PetscReal L[]) {
+  return (-Sqr(PetscTanhReal(x[0]/L[0])) + 1)/L[0];}
+static PetscReal Bend_xx0(const PetscReal x[],const PetscReal L[]) {
+  return -(-2*Sqr(PetscTanhReal(x[0]/L[0])) + 2)*PetscTanhReal(x[0]/L[0])/Sqr(L[0]);}
+static PetscReal Bend_x1(const PetscReal x[],const PetscReal L[]) {
+  return 1/(L[1]*(1 + x[1]/L[1]));}
+static PetscReal Bend_xx1(const PetscReal x[],const PetscReal L[]) {
+  return -1/(Sqr(L[1])*Sqr(1 + x[1]/L[1]));}
+static PetscReal Bend_x2(const PetscReal x[],const PetscReal L[]) {
+  return -PetscExpReal(-x[2]/L[2])/L[2];}
+static PetscReal Bend_xx2(const PetscReal x[],const PetscReal L[]) {
+  return PetscExpReal(-x[2]/L[2])/Sqr(L[2]);}
+static PetscErrorCode OpPointwiseSolution_Poisson_Hump(Op op,const PetscReal x[],const PetscReal L[],PetscScalar u[]) {
+  u[0] = Hump(x,L) * Bend(x,L);
+  return 0;
+}
+// (f g)'' = f'' g + 2 f' g' + f g''
+static PetscErrorCode OpPointwiseForcing_Poisson_Hump(Op op,const PetscReal x[],const PetscReal L[],PetscScalar f[]) {
+  f[0] = -(  Hump_xx0(x,L)*Bend(x,L) + 2*Hump_x0(x,L)*Bend_x0(x,L) + Hump(x,L)*Bend_xx0(x,L)
+           + Hump_xx1(x,L)*Bend(x,L) + 2*Hump_x1(x,L)*Bend_x1(x,L) + Hump(x,L)*Bend_xx1(x,L)
+           + Hump_xx2(x,L)*Bend(x,L) + 2*Hump_x2(x,L)*Bend_x2(x,L) + Hump(x,L)*Bend_xx2(x,L));
+  return 0;
+}
+
+static PetscReal Wave(const PetscReal x) {
+  return x*x*x*x - x*x + 2*x*x*x - 2*x*x*x*x*x;}
+static PETSC_UNUSED PetscReal Wave_x(const PetscReal x) {
+  return 4*x*x*x - 2*x + 6*x*x - 10*x*x*x*x;}
+static PetscReal Wave_xx(const PetscReal x) {
+  return 12*x*x - 2 + 12*x - 40*x*x*x;}
+static PetscErrorCode OpPointwiseSolution_Poisson_Wave(Op op,const PetscReal x[],const PetscReal L[],PetscScalar u[]) {
+  u[0] = Wave(x[0]/L[0]) * Wave(x[1]/L[1]) * Wave(x[2]/L[2]);
+  return 0;
+}
+static PetscErrorCode OpPointwiseForcing_Poisson_Wave(Op op,const PetscReal x[],const PetscReal L[],PetscScalar f[]) {
+  f[0] = -(  Wave_xx(x[0]/L[0]) * Wave   (x[1]/L[1]) * Wave   (x[2]/L[2])/Sqr(L[0])
+           + Wave   (x[0]/L[0]) * Wave_xx(x[1]/L[1]) * Wave   (x[2]/L[2])/Sqr(L[1])
+           + Wave   (x[0]/L[0]) * Wave   (x[1]/L[1]) * Wave_xx(x[2]/L[2])/Sqr(L[2]));
+  return 0;
+}
+
+static inline PetscErrorCode OpPointwiseElement_PoissonN(Op op,PetscInt ne,PetscInt Q3,PetscScalar dx[3][3][Q3][NE],PetscReal wdxdet[Q3][NE],PetscScalar du[3][1][Q3][NE],PetscScalar dv[3][1][Q3][NE]) {
   for (PetscInt i=0; i<Q3; i++) {
-    for (PetscInt e=0; e<NE; e++) {
+    for (PetscInt e=0; e<ne; e++) {
       PetscScalar dux[3][1],dvx[3][1];
       for (PetscInt k=0; k<3; k++) {
         dux[k][0] = du[0][0][i][e] * dx[k][0][i][e] + du[1][0][i][e] * dx[k][1][i][e] + du[2][0][i][e] * dx[k][2][i][e];
@@ -34,11 +94,18 @@ static PetscErrorCode OpPointwiseElement_Poisson1(Op op,PetscInt ne,PetscInt Q3,
       }
     }
   }
-  PetscLogFlops(Q3*NE*(5*3 + 0 + 6*3));
+  PetscLogFlops(Q3*ne*(5*3 + 0 + 6*3));
   return 0;
 }
+// Let the compiler specialize on known array sizes
+static PetscErrorCode OpPointwiseElement_Poisson1(Op op,PetscInt ne,PetscInt Q3,PetscScalar dx[3][3][Q3][NE],PetscReal wdxdet[Q3][NE],PetscScalar du[3][1][Q3][NE],PetscScalar dv[3][1][Q3][NE]) {
+  return OpPointwiseElement_PoissonN(op,NE,8,dx,wdxdet,du,dv);}
+static PetscErrorCode OpPointwiseElement_Poisson2(Op op,PetscInt ne,PetscInt Q3,PetscScalar dx[3][3][Q3][NE],PetscReal wdxdet[Q3][NE],PetscScalar du[3][1][Q3][NE],PetscScalar dv[3][1][Q3][NE]) {
+  return OpPointwiseElement_PoissonN(op,NE,27,dx,wdxdet,du,dv);}
 
-static PetscErrorCode OpApply_Poisson1(Op op,DM dm,Vec U,Vec V)
+
+static PetscErrorCode OpApply_Poisson(Op op,DM dm,Vec U,Vec V,
+                                      PetscErrorCode (*PointwiseElement)(Op op,PetscInt ne,PetscInt Q3,PetscScalar dx[3][3][Q3][NE],PetscReal wdxdet[Q3][NE],PetscScalar du[3][1][Q3][NE],PetscScalar dv[3][1][Q3][NE]))
 {
   PetscErrorCode ierr;
   Vec X,Ul,Vl;
@@ -79,7 +146,7 @@ static PetscErrorCode OpApply_Poisson1(Op op,DM dm,Vec U,Vec V)
     ierr = TensorContract(NE,1,P,Q,D,B,B,TENSOR_EVAL,ue,du[0][0][0]);CHKERRQ(ierr);
     ierr = TensorContract(NE,1,P,Q,B,D,B,TENSOR_EVAL,ue,du[1][0][0]);CHKERRQ(ierr);
     ierr = TensorContract(NE,1,P,Q,B,B,D,TENSOR_EVAL,ue,du[2][0][0]);CHKERRQ(ierr);
-    ierr = OpPointwiseElement_Poisson1(op,NE,Q3,dx,wdxdet,du,dv);CHKERRQ(ierr);
+    ierr = PointwiseElement(op,NE,Q3,dx,wdxdet,du,dv);CHKERRQ(ierr);
     ierr = PetscMemzero(ve,sizeof ve);CHKERRQ(ierr);
     ierr = TensorContract(NE,1,P,Q,D,B,B,TENSOR_TRANSPOSE,dv[0][0][0],ve);CHKERRQ(ierr);
     ierr = TensorContract(NE,1,P,Q,B,D,B,TENSOR_TRANSPOSE,dv[1][0][0],ve);CHKERRQ(ierr);
@@ -96,8 +163,13 @@ static PetscErrorCode OpApply_Poisson1(Op op,DM dm,Vec U,Vec V)
   ierr = DMRestoreLocalVector(dm,&Vl);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
+// Not trying to inline yet because Q1 is faster without tensor product, so it's really worth specializing the whole
+// element loop.  The non-tensor code is slightly simpler, but not implemented yet, so delay.  I don't think we'll end
+// up using Q1 anyway at the end of the day.
+static PetscErrorCode OpApply_Poisson1(Op op,DM dm,Vec U,Vec V) { return OpApply_Poisson(op,dm,U,V,OpPointwiseElement_Poisson1); }
+static PetscErrorCode OpApply_Poisson2(Op op,DM dm,Vec U,Vec V) { return OpApply_Poisson(op,dm,U,V,OpPointwiseElement_Poisson2); }
 
-static PetscErrorCode OpDestroy_Poisson1(Op op)
+static PetscErrorCode OpDestroy_Poisson(Op op)
 {
   PetscErrorCode ierr;
   Ctx ctx;
@@ -108,20 +180,52 @@ static PetscErrorCode OpDestroy_Poisson1(Op op)
   PetscFunctionReturn(0);
 }
 
-PetscErrorCode OpCreate_Poisson1(Op op)
+static PetscErrorCode OpCreate__Poisson(Op op)
 {
   PetscErrorCode ierr;
   Ctx ctx;
+  char solname[256] = "wave";
 
   PetscFunctionBegin;
+  ierr = PetscOptionsBegin(OpComm(op),NULL,"Poisson Options",NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsString("-poisson_solution","Name of manufactured/analytic solution","",solname,solname,sizeof solname,NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsEnd();CHKERRQ(ierr);
+  if (!strcasecmp(solname,"sine")) {
+    ierr = OpSetPointwiseSolution(op,OpPointwiseSolution_Poisson_Sine);CHKERRQ(ierr);
+    ierr = OpSetPointwiseForcing(op,OpPointwiseForcing_Poisson_Sine);CHKERRQ(ierr);
+  } else if (!strcasecmp(solname,"hump")) {
+    ierr = OpSetPointwiseSolution(op,OpPointwiseSolution_Poisson_Hump);CHKERRQ(ierr);
+    ierr = OpSetPointwiseForcing(op,OpPointwiseForcing_Poisson_Hump);CHKERRQ(ierr);
+  } else if (!strcasecmp(solname,"wave")) {
+    ierr = OpSetPointwiseSolution(op,OpPointwiseSolution_Poisson_Wave);CHKERRQ(ierr);
+    ierr = OpSetPointwiseForcing(op,OpPointwiseForcing_Poisson_Wave);CHKERRQ(ierr);
+  }
   ierr = PetscNew(&ctx);CHKERRQ(ierr);
   ierr = OpSetDof(op,1);CHKERRQ(ierr);
-  ierr = OpSetFEDegree(op,1);CHKERRQ(ierr);
   ierr = OpSetContext(op,ctx);CHKERRQ(ierr);
-  ierr = OpSetPointwiseSolution(op,OpPointwiseSolution_Poisson1);CHKERRQ(ierr);
-  ierr = OpSetPointwiseForcing(op,OpPointwiseForcing_Poisson1);CHKERRQ(ierr);
-  ierr = OpSetPointwiseElement(op,(OpPointwiseElementFunction)OpPointwiseElement_Poisson1,NE);CHKERRQ(ierr);
+  ierr = OpSetDestroy(op,OpDestroy_Poisson);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+
+PetscErrorCode OpCreate_Poisson1(Op op)
+{
+  PetscErrorCode ierr;
+
+  PetscFunctionBegin;
+  ierr = OpCreate__Poisson(op);CHKERRQ(ierr);
+  ierr = OpSetFEDegree(op,1);CHKERRQ(ierr);
   ierr = OpSetApply(op,OpApply_Poisson1);CHKERRQ(ierr);
-  ierr = OpSetDestroy(op,OpDestroy_Poisson1);CHKERRQ(ierr);
+  ierr = OpSetPointwiseElement(op,(OpPointwiseElementFunction)OpPointwiseElement_Poisson1,NE);CHKERRQ(ierr);
+  PetscFunctionReturn(0);
+}
+PetscErrorCode OpCreate_Poisson2(Op op)
+{
+  PetscErrorCode ierr;
+
+  PetscFunctionBegin;
+  ierr = OpCreate__Poisson(op);CHKERRQ(ierr);
+  ierr = OpSetFEDegree(op,2);CHKERRQ(ierr);
+  ierr = OpSetApply(op,OpApply_Poisson2);CHKERRQ(ierr);
+  ierr = OpSetPointwiseElement(op,(OpPointwiseElementFunction)OpPointwiseElement_Poisson2,NE);CHKERRQ(ierr);
   PetscFunctionReturn(0);
 }
--- impls/fefas/test/t200-mgv.sh
+++ impls/fefas/test/t200-mgv.sh
@@ -5,7 +5,7 @@ test_description='Test Poisson solve using MG V-cycles'
 . ./fefas-sharness.sh
 
 # Error norm is converging at second order
-test_expect_stdout 'FE Poisson MG V-cycle solve fedegree=1 serial' 1 'fefas mgv -op_type poisson1 -M 16,20,24 -L 1,1,1 -smooth 1,1 -mg_eig_target 2,0.2' '
+test_expect_stdout 'FE Poisson MG V-cycle solve fedegree=1 serial' 1 'fefas mgv -op_type poisson1 -M 16,20,24 -L 1,1,1 -smooth 1,1 -mg_eig_target 2,0.2 -poisson_solution sine' '
 [0] Level 2: [   0:  16,   0:  20,   0:  24] of [  16,  20,  24] on [  1,  1,  1]
 [0] Level 1: [   0:   8,   0:  10,   0:  12] of [   8,  10,  12] on [  1,  1,  1]
 [0] Level 0: [   0:   4,   0:   5,   0:   6] of [   4,   5,   6] on [  1,  1,  1]
@@ -16,7 +16,7 @@ V(1,1)  3: |e|_2/|u|_2 1.34e-02  |r|_2/|f|_2 3.22e-03
 V(1,1)  4: |e|_2/|u|_2 1.35e-02  |r|_2/|f|_2 7.91e-04
 '
 
-test_expect_stdout 'FE Poisson MG V-cycle solve fedegree=1 parallel' 4 'fefas mgv -op_type poisson1 -M 16,20,24 -L 1,1,1 -p 1,2,2 -cmax 240 -smooth 1,1 -mg_eig_target 2,0.2' '
+test_expect_stdout 'FE Poisson MG V-cycle solve fedegree=1 parallel' 4 'fefas mgv -op_type poisson1 -M 16,20,24 -L 1,1,1 -p 1,2,2 -cmax 240 -smooth 1,1 -mg_eig_target 2,0.2 -poisson_solution sine' '
 [0] Level 2: [   0:  16,   0:  12,   0:  12] of [  16,  20,  24] on [  1,  2,  2]
 [0] Level 1: [   0:   8,   0:   6,   0:   6] of [   8,  10,  12] on [  1,  2,  2]
 [0] Level 0: [   0:   4,   0:   5,   0:   6] of [   4,   5,   6] on [  1,  1,  1]
