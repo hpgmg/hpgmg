da1871b3278eea8576ddedffad0a556fd04b7018
Jed Brown <jed@jedbrown.org>
2014-04-05 17:44:38 -0600

    fefas-analyze: report dofs instead of elements, add --perprocess

--- impls/fefas/fefas-analyze.py
+++ impls/fefas/fefas-analyze.py
@@ -3,7 +3,7 @@ def parse_logfile(fname):
     FP = r'([+-]?(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][+-]?\d+)?)'
     PERFLINE = re.compile(r'Q2 G''\[([\d ]{4})([\d ]{4})([\d ]{4})\] P\[ *(\d+) +(\d+) +(\d+)\]  '+FP+r' s +'+FP+r' GF +'+FP+r' MEq/s')
     HOSTLINE = re.compile(r'.*on a ([a-z\-_0-9]+) named \w+ with (\d+) processors')
-    Sizes = []
+    Dofs = []
     GFlops = []
     MEqs = []
     Procs = None
@@ -17,9 +17,9 @@ def parse_logfile(fname):
             if not m:
                 break
             g0,g1,g2, p0,p1,p2, time, gflops, meqs = m.groups()
-            g = float(g0)*float(g1)*float(g2)
+            g = (float(g0)*2+1)*(float(g1)*2+1)*(float(g2)*2+1)
             p = int(p0)*int(p1)*int(p2)
-            Sizes.append(g)
+            Dofs.append(g)
             GFlops.append(float(gflops))
             MEqs.append(float(meqs))
             if Procs is None:
@@ -34,33 +34,38 @@ def parse_logfile(fname):
                 assert int(p) == Procs
                 break
 
-    return Sizes, GFlops, MEqs, HostName, Procs
+    return Dofs, GFlops, MEqs, HostName, Procs
 
 def plot(args):
     symbols = iter(['ro', 'bv', 'ks', 'g^'])
     import matplotlib.pyplot as plt
     fig, ax1 = plt.subplots()
     plt.title('FE-FAS Performance')
-    plt.xlabel('Global number of elements')
+    if args.perprocess:
+        plt.xlabel('Number of equations/process')
+    else:
+        plt.xlabel('Global number of equations')
     ax2 = ax1.twinx()
     #ax1.set_autoscaley_on(False)
     ax1.set_ylabel('MEquations/second')
-    all_sizes = []
+    all_dofs = []
     all_gflops = []
     all_meqs = []
     max_meqs = 0
     for f in args.logfiles:
-        sizes, gflops, meqs, hostname, procs = parse_logfile(f)
-        all_sizes += sizes
+        dofs, gflops, meqs, hostname, procs = parse_logfile(f)
+        if args.perprocess:
+            dofs = [d/procs for d in dofs]
+        all_dofs += dofs
         all_gflops += gflops
         all_meqs += meqs
         if args.loglog:
-            ax1.loglog(sizes, meqs, next(symbols), label='%s np=%d'%(hostname, procs))
+            ax1.loglog(dofs, meqs, next(symbols), label='%s np=%d'%(hostname, procs))
         else:
-            ax1.semilogx(sizes, meqs, next(symbols), label='%s np=%d'%(hostname, procs))
+            ax1.semilogx(dofs, meqs, next(symbols), label='%s np=%d'%(hostname, procs))
     flops_per_meqn = all_gflops[-1] / all_meqs[-1]
-    ax1.set_xlim(0.9*min(all_sizes),1.05*max(all_sizes))
-    ax2.set_xlim(0.9*min(all_sizes),1.05*max(all_sizes))
+    ax1.set_xlim(0.9*min(all_dofs),1.05*max(all_dofs))
+    ax2.set_xlim(0.9*min(all_dofs),1.05*max(all_dofs))
     ax2.set_autoscaley_on(False)
     if args.loglog:
         ax2.set_yscale('log')
@@ -79,7 +84,8 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser('FE-FAS Performance Analyzer')
     parser.add_argument('-o', '--output', type=str, help='Output file')
-    parser.add_argument('--loglog', type=bool, help='Use logarithmic y axis (x is always logarithmic)')
+    parser.add_argument('--loglog', action='store_true', help='Use logarithmic y axis (x is always logarithmic)')
+    parser.add_argument('--perprocess', action='store_true', help='Use problem size per process for x axis')
     parser.add_argument('logfiles', nargs='+', type=str, help='List of files to process, usually including -log_summary')
     args = parser.parse_args()
     plot(args)
