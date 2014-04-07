4abeecfb08c914e842cfabd028bf39df92ea6d3b
Jed Brown <jed@jedbrown.org>
2014-04-06 19:17:43 -0600

    config: use argparse groups and add support for Sam's other timers

--- hpgmgconf.py
+++ hpgmgconf.py
@@ -1,5 +1,6 @@
+import sys,os
+
 def mkdir_p(path):
-    import os
     try:
         os.makedirs(path)
     except OSError as exc:
@@ -21,11 +22,11 @@ def main():
     fv = parser.add_argument_group('Finite Volume options')
     fv.add_argument('--no-fv', action='store_false', dest='fv', help='Do not build the Finite-Volume solver')
     fv.add_argument('--fv-mpi', action='store_true', help='Use MPI', default=True)
-    fv.add_argument('--fv-cycle', help='Use U-, V-, or F-cycles', default='F', type=str)
-    fv.add_argument('--fv-bicgstab', action='store_true', help='Use BiCGStab as a bottom (coarse grid) solver', default=False)
+    fv.add_argument('--fv-cycle', help='Multigrid cycle type', choices=['V','F','W'], default='F')
     fv.add_argument('--fv-subcomm', action='store_true', help='build a subcommunicator for each level in the MG v-cycle to minimize the scope of MPI_AllReduce()', default=True)
-    fv.add_argument('--fv-smoother', help='Use Cheby, GSRB, Jacobi, or L1Jacobi for smoother', default='Cheby', type=str)
-    fv.add_argument('--fv-timer', help='Use "x86" or "bgq" timers', default='x86', type=str);
+    fv.add_argument('--fv-coarse-solver', help='Use BiCGStab as a bottom (coarse grid) solver', choices=['bicgstab','cabicgstab','cg','cacg'], default='bicgstab')
+    fv.add_argument('--fv-smoother', help='Multigrid smoother', choices=['cheby','gsrb','jacobi','l1jacobi'], default='cheby')
+    fv.add_argument('--fv-timer', help='Timer implementation', choices=['x86','omp','mpi','bgq','sparc'], default='mpi')
     args = parser.parse_args()
     if args.arch is None:
         args.arch = args.petsc_arch
@@ -35,26 +36,10 @@ def main():
     configure(args)
 
 def configure(args):
-    import sys,os
     mkdir_p(os.path.join(args.arch, 'include'))
     with open(os.path.join(args.arch, 'include', 'hpgmgconf.h'), 'w') as f:
         f.write(hpgmgconf_h(args))
-    open(os.path.join(args.arch,'Makefile'), 'w').write('\n'.join([
-        'HPGMG_ARCH = %s' % args.arch,
-        'HPGMG_CC = %s' % args.CC,
-        'HPGMG_CFLAGS = %s' % args.CFLAGS,
-        'HPGMG_LDFLAGS = %s' % args.LDFLAGS,
-        'HPGMG_LDLIBS = %s' % args.LDLIBS,
-        'PETSC_DIR = %s' % args.petsc_dir,
-        'PETSC_ARCH = %s' % args.petsc_arch,
-        'PYTHON = %s' % sys.executable,
-        'SRCDIR = %s' % os.path.abspath(os.path.dirname(__name__)),
-        'CONFIG_FV = %s' % ('y' if args.fv else ''),
-        'CONFIG_X86 = %s' % ('y' if args.fv_timer == 'x86' else ''),
-        'CONFIG_BGQ = %s' % ('y' if args.fv_timer == 'bgq' else ''),
-        ('# ' if not args.petsc_dir else '') + 'include $(PETSC_DIR)/conf/variables',
-        'include $(SRCDIR)/base.mk',
-    ]))
+    open(os.path.join(args.arch,'Makefile'), 'w').write(makefile(args))
     reconfname = os.path.join(args.arch,'reconfigure-%s.py' % args.arch)
     open(reconfname, 'w').write('\n'.join([
                 '#!'+sys.executable,
@@ -72,8 +57,7 @@ def hpgmgconf_h(args):
     defines = []
     if args.fv_mpi:
         defines.append('MPI')
-    if args.fv_bicgstab:
-        defines.append('USE_BICGSTAB')
+    defines.append('USE_%s' % args.fv_coarse_solver.upper())
     if args.fv_subcomm:
         defines.append('USE_SUBCOMM')
     defines.append('USE_%sCYCLES' % args.fv_cycle.upper())
@@ -83,3 +67,21 @@ def hpgmgconf_h(args):
 %s
 #endif
 """ % '\n'.join('#define __%s 1'%d for d in defines)
+
+def makefile(args):
+    m = ['HPGMG_ARCH = %s' % args.arch,
+        'HPGMG_CC = %s' % args.CC,
+        'HPGMG_CFLAGS = %s' % args.CFLAGS,
+        'HPGMG_LDFLAGS = %s' % args.LDFLAGS,
+        'HPGMG_LDLIBS = %s' % args.LDLIBS,
+        'PETSC_DIR = %s' % args.petsc_dir,
+        'PETSC_ARCH = %s' % args.petsc_arch,
+        'PYTHON = %s' % sys.executable,
+        'SRCDIR = %s' % os.path.abspath(os.path.dirname(__name__)),]
+    if args.fv:
+        m.append('CONFIG_FV = y')
+    m.append('CONFIG_TIMER_%s = y' % args.fv_timer.upper())
+    if args.petsc_dir:
+        m.append('include $(PETSC_DIR)/conf/variables')
+    m.append('include $(SRCDIR)/base.mk\n')
+    return '\n'.join(m)
