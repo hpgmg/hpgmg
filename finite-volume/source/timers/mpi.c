78bb4c176b0a0e6f789f0f6152f71d0395839ae2
Sam Williams <swwilliams@lbl.gov>
2014-07-09 10:24:20 -0700

    unified timers

--- finite-volume/source/timers/mpi.c
+++ finite-volume/source/timers/mpi.c
@@ -3,10 +3,8 @@
 // SWWilliams@lbl.gov
 // Lawrence Berkeley National Lab
 //------------------------------------------------------------------------------------------------------------------------------
-#ifdef _OPENMP
-#include "./timers/omp.c"
-#elif USE_MPI
-#include "./timers/mpi.c"
-#else
-#error You need to include a custom timer routine
-#endif
+#include <stdint.h>
+#include <mpi.h>
+uint64_t CycleTime(){
+  return((uint64_t)(1e9*MPI_Wtime())); // convert DP time in seconds to 64b integer nanosecond counter...
+}
--- finite-volume/source/timers/omp.c
+++ finite-volume/source/timers/omp.c
@@ -3,10 +3,8 @@
 // SWWilliams@lbl.gov
 // Lawrence Berkeley National Lab
 //------------------------------------------------------------------------------------------------------------------------------
-#ifdef _OPENMP
-#include "./timers/omp.c"
-#elif USE_MPI
-#include "./timers/mpi.c"
-#else
-#error You need to include a custom timer routine
-#endif
+#include <stdint.h>
+#include <omp.h>
+uint64_t CycleTime(){
+  return((uint64_t)(1e9*omp_get_wtime())); // convert DP time in seconds to 64b integer nanosecond counter...
+}
