a8564f4db7efc960a1ce174bc5ef92ef90d9675a
Sam Williams <swwilliams@lbl.gov>
2014-08-21 10:18:01 -0700

    fixed bug in linear BC's

--- finite-volume/source/operators/boundary_conditions.c
+++ finite-volume/source/operators/boundary_conditions.c
@@ -27,7 +27,6 @@ void apply_BCs_linear(level_type * level, int x_id){
 
   #pragma omp parallel for private(box) OMP_THREAD_ACROSS_BOXES(level->concurrent_boxes)
   for(box=0;box<level->num_my_boxes;box++){
-    int i,j,k;
     int jStride = level->my_boxes[box].jStride;
     int kStride = level->my_boxes[box].kStride;
     int  ghosts = level->my_boxes[box].ghosts;
@@ -35,36 +34,53 @@ void apply_BCs_linear(level_type * level, int x_id){
     double * __restrict__ x      = level->my_boxes[box].vectors[        x_id] + ghosts*(1+jStride+kStride); // i.e. [0] = first non ghost zone point
   //double * __restrict__  valid = level->my_boxes[box].vectors[VECTOR_VALID] + ghosts*(1+jStride+kStride);
 
+    int box_on_low_i  = (level->my_boxes[box].low.i     ==            0);
+    int box_on_low_j  = (level->my_boxes[box].low.j     ==            0);
+    int box_on_low_k  = (level->my_boxes[box].low.k     ==            0);
+    int box_on_high_i = (level->my_boxes[box].low.i+dim == level->dim.i);
+    int box_on_high_j = (level->my_boxes[box].low.j+dim == level->dim.j);
+    int box_on_high_k = (level->my_boxes[box].low.k+dim == level->dim.k);
+
     if(level->domain_boundary_condition == BC_DIRICHLET){
-      // why these and not -1, -5, +77 ???
-                  k= -1;if((level->my_boxes[box].low.k     ==            0))                                                                for(j=0;j<dim;j++)for(i=0;i<dim;i++){int ijk=i+j*jStride+k*kStride;x[ijk]=-x[ijk          +kStride];} // face
-            j= -1;      if((level->my_boxes[box].low.j     ==            0))                                                                for(k=0;k<dim;k++)for(i=0;i<dim;i++){int ijk=i+j*jStride+k*kStride;x[ijk]=-x[ijk  +jStride        ];} // face
-      i= -1;            if((level->my_boxes[box].low.i     ==            0))                                                                for(k=0;k<dim;k++)for(j=0;j<dim;j++){int ijk=i+j*jStride+k*kStride;x[ijk]=-x[ijk+1                ];} // face
-      i=dim;            if((level->my_boxes[box].low.i+dim == level->dim.i))                                                                for(k=0;k<dim;k++)for(j=0;j<dim;j++){int ijk=i+j*jStride+k*kStride;x[ijk]=-x[ijk-1                ];} // face
-            j=dim;      if((level->my_boxes[box].low.j+dim == level->dim.j))                                                                for(k=0;k<dim;k++)for(i=0;i<dim;i++){int ijk=i+j*jStride+k*kStride;x[ijk]=-x[ijk  -jStride        ];} // face
-                  k=dim;if((level->my_boxes[box].low.k+dim == level->dim.k))                                                                for(j=0;j<dim;j++)for(i=0;i<dim;i++){int ijk=i+j*jStride+k*kStride;x[ijk]=-x[ijk          -kStride];} // face
+      int i,j,k,normal;
+      double s;
 
-            j= -1;k= -1;if((level->my_boxes[box].low.j     ==            0)&&(level->my_boxes[box].low.k     ==            0))                                for(i=0;i<dim;i++){int ijk=i+j*jStride+k*kStride;x[ijk]=x[ijk  +jStride+kStride];} // edge
-      i= -1;      k= -1;if((level->my_boxes[box].low.i     ==            0)&&(level->my_boxes[box].low.k     ==            0))                                for(j=0;j<dim;j++){int ijk=i+j*jStride+k*kStride;x[ijk]=x[ijk+1        +kStride];} // edge
-      i=dim;      k= -1;if((level->my_boxes[box].low.i+dim == level->dim.i)&&(level->my_boxes[box].low.k     ==            0))                                for(j=0;j<dim;j++){int ijk=i+j*jStride+k*kStride;x[ijk]=x[ijk-1        +kStride];} // edge
-            j=dim;k= -1;if((level->my_boxes[box].low.j+dim == level->dim.j)&&(level->my_boxes[box].low.k     ==            0))                                for(i=0;i<dim;i++){int ijk=i+j*jStride+k*kStride;x[ijk]=x[ijk  -jStride+kStride];} // edge
-      i= -1;j= -1;      if((level->my_boxes[box].low.i     ==            0)&&(level->my_boxes[box].low.j     ==            0))                                for(k=0;k<dim;k++){int ijk=i+j*jStride+k*kStride;x[ijk]=x[ijk+1+jStride        ];} // edge
-      i=dim;j= -1;      if((level->my_boxes[box].low.i+dim == level->dim.i)&&(level->my_boxes[box].low.j     ==            0))                                for(k=0;k<dim;k++){int ijk=i+j*jStride+k*kStride;x[ijk]=x[ijk-1+jStride        ];} // edge
-      i= -1;j=dim;      if((level->my_boxes[box].low.i     ==            0)&&(level->my_boxes[box].low.j+dim == level->dim.j))                                for(k=0;k<dim;k++){int ijk=i+j*jStride+k*kStride;x[ijk]=x[ijk+1-jStride        ];} // edge
-      i=dim;j=dim;      if((level->my_boxes[box].low.i+dim == level->dim.i)&&(level->my_boxes[box].low.j+dim == level->dim.j))                                for(k=0;k<dim;k++){int ijk=i+j*jStride+k*kStride;x[ijk]=x[ijk-1-jStride        ];} // edge
-            j= -1;k=dim;if((level->my_boxes[box].low.j     ==            0)&&(level->my_boxes[box].low.k+dim == level->dim.k))                                for(i=0;i<dim;i++){int ijk=i+j*jStride+k*kStride;x[ijk]=x[ijk  +jStride-kStride];} // edge
-      i= -1;      k=dim;if((level->my_boxes[box].low.i     ==            0)&&(level->my_boxes[box].low.k+dim == level->dim.k))                                for(j=0;j<dim;j++){int ijk=i+j*jStride+k*kStride;x[ijk]=x[ijk+1        -kStride];} // edge
-      i=dim;      k=dim;if((level->my_boxes[box].low.i+dim == level->dim.i)&&(level->my_boxes[box].low.k+dim == level->dim.k))                                for(j=0;j<dim;j++){int ijk=i+j*jStride+k*kStride;x[ijk]=x[ijk-1        -kStride];} // edge
-            j=dim;k=dim;if((level->my_boxes[box].low.j+dim == level->dim.j)&&(level->my_boxes[box].low.k+dim == level->dim.k))                                for(i=0;i<dim;i++){int ijk=i+j*jStride+k*kStride;x[ijk]=x[ijk  -jStride-kStride];} // edge
+      // note, just because you are in a corner ghost zone, doesn't mean you are on the corner of the domain.
+      // thus, one needs to calculate the normal to the domain (not normal to box) in each ghost zone region
+      // depending on whether this normal is on a domain face, edge, or corner, one needs to choose 's' appropriately
 
-      i= -1;j= -1;k= -1;if((level->my_boxes[box].low.i     ==            0)&&(level->my_boxes[box].low.j     ==            0)&&(level->my_boxes[box].low.k     ==            0)){int ijk=i+j*jStride+k*kStride;x[ijk]=-x[ijk+1+jStride+kStride];} // corner
-      i=dim;j= -1;k= -1;if((level->my_boxes[box].low.i+dim == level->dim.i)&&(level->my_boxes[box].low.j     ==            0)&&(level->my_boxes[box].low.k     ==            0)){int ijk=i+j*jStride+k*kStride;x[ijk]=-x[ijk-1+jStride+kStride];} // corner
-      i= -1;j=dim;k= -1;if((level->my_boxes[box].low.i     ==            0)&&(level->my_boxes[box].low.j+dim == level->dim.j)&&(level->my_boxes[box].low.k     ==            0)){int ijk=i+j*jStride+k*kStride;x[ijk]=-x[ijk+1-jStride+kStride];} // corner
-      i=dim;j=dim;k= -1;if((level->my_boxes[box].low.i+dim == level->dim.i)&&(level->my_boxes[box].low.j+dim == level->dim.j)&&(level->my_boxes[box].low.k     ==            0)){int ijk=i+j*jStride+k*kStride;x[ijk]=-x[ijk-1-jStride+kStride];} // corner
-      i= -1;j= -1;k=dim;if((level->my_boxes[box].low.i     ==            0)&&(level->my_boxes[box].low.j     ==            0)&&(level->my_boxes[box].low.k+dim == level->dim.k)){int ijk=i+j*jStride+k*kStride;x[ijk]=-x[ijk+1+jStride-kStride];} // corner
-      i=dim;j= -1;k=dim;if((level->my_boxes[box].low.i+dim == level->dim.i)&&(level->my_boxes[box].low.j     ==            0)&&(level->my_boxes[box].low.k+dim == level->dim.k)){int ijk=i+j*jStride+k*kStride;x[ijk]=-x[ijk-1+jStride-kStride];} // corner
-      i= -1;j=dim;k=dim;if((level->my_boxes[box].low.i     ==            0)&&(level->my_boxes[box].low.j+dim == level->dim.j)&&(level->my_boxes[box].low.k+dim == level->dim.k)){int ijk=i+j*jStride+k*kStride;x[ijk]=-x[ijk+1-jStride-kStride];} // corner
-      i=dim;j=dim;k=dim;if((level->my_boxes[box].low.i+dim == level->dim.i)&&(level->my_boxes[box].low.j+dim == level->dim.j)&&(level->my_boxes[box].low.k+dim == level->dim.k)){int ijk=i+j*jStride+k*kStride;x[ijk]=-x[ijk-1-jStride-kStride];} // corner
+      // calculate a normal vector for this face                                                              // if face is on a domain boundary, impose the boundary condition using the calculated normal
+      s=1;if(box_on_low_i ){normal= 1+      0+      0;s*=-1;}                                                           if(box_on_low_i ){i= -1;j  =0;k  =0;for(j=0;j<dim;j++)for(k=0;k<dim;k++){int ijk=i+j*jStride+k*kStride;x[ijk]=s*x[ijk+normal];}}
+      s=1;if(box_on_low_j ){normal= 0+jStride+      0;s*=-1;}                                                           if(box_on_low_j ){i=  0;j= -1;k  =0;for(k=0;k<dim;k++)for(i=0;i<dim;i++){int ijk=i+j*jStride+k*kStride;x[ijk]=s*x[ijk+normal];}}
+      s=1;if(box_on_low_k ){normal= 0+      0+kStride;s*=-1;}                                                           if(box_on_low_k ){i=  0;j  =0;k= -1;for(j=0;j<dim;j++)for(i=0;i<dim;i++){int ijk=i+j*jStride+k*kStride;x[ijk]=s*x[ijk+normal];}}
+      s=1;if(box_on_high_i){normal=-1+      0+      0;s*=-1;}                                                           if(box_on_high_i){i=dim;j  =0;k  =0;for(j=0;j<dim;j++)for(k=0;k<dim;k++){int ijk=i+j*jStride+k*kStride;x[ijk]=s*x[ijk+normal];}}
+      s=1;if(box_on_high_j){normal= 0-jStride+      0;s*=-1;}                                                           if(box_on_high_j){i=  0;j=dim;k  =0;for(k=0;k<dim;k++)for(i=0;i<dim;i++){int ijk=i+j*jStride+k*kStride;x[ijk]=s*x[ijk+normal];}}
+      s=1;if(box_on_high_k){normal= 0+      0-kStride;s*=-1;}                                                           if(box_on_high_k){i=  0;j  =0;k=dim;for(j=0;j<dim;j++)for(i=0;i<dim;i++){int ijk=i+j*jStride+k*kStride;x[ijk]=s*x[ijk+normal];}}
+
+      // calculate a normal vector for this edge                                                                                              // if edge is on a domain boundary, impose the boundary condition using the calculated normal
+      s=1;normal=0;if(box_on_low_j ){normal+=jStride;s*=-1;}if(box_on_low_k ){normal+=kStride;s*=-1;}                                         if(box_on_low_j ||box_on_low_k ){i=  0;j= -1;k= -1;for(i=0;i<dim;i++){int ijk=i+j*jStride+k*kStride;x[ijk]=s*x[ijk+normal];}}
+      s=1;normal=0;if(box_on_high_j){normal-=jStride;s*=-1;}if(box_on_low_k ){normal+=kStride;s*=-1;}                                         if(box_on_high_j||box_on_low_k ){i=  0;j=dim;k= -1;for(i=0;i<dim;i++){int ijk=i+j*jStride+k*kStride;x[ijk]=s*x[ijk+normal];}}
+      s=1;normal=0;if(box_on_low_j ){normal+=jStride;s*=-1;}if(box_on_high_k){normal-=kStride;s*=-1;}                                         if(box_on_low_j ||box_on_high_k){i=  0;j= -1;k=dim;for(i=0;i<dim;i++){int ijk=i+j*jStride+k*kStride;x[ijk]=s*x[ijk+normal];}}
+      s=1;normal=0;if(box_on_high_j){normal-=jStride;s*=-1;}if(box_on_high_k){normal-=kStride;s*=-1;}                                         if(box_on_high_j||box_on_high_k){i=  0;j=dim;k=dim;for(i=0;i<dim;i++){int ijk=i+j*jStride+k*kStride;x[ijk]=s*x[ijk+normal];}}
+      s=1;normal=0;if(box_on_low_i ){normal+=      1;s*=-1;}if(box_on_low_k ){normal+=kStride;s*=-1;}                                         if(box_on_low_i ||box_on_low_k ){i= -1;j=  0;k= -1;for(j=0;j<dim;j++){int ijk=i+j*jStride+k*kStride;x[ijk]=s*x[ijk+normal];}}
+      s=1;normal=0;if(box_on_high_i){normal-=      1;s*=-1;}if(box_on_low_k ){normal+=kStride;s*=-1;}                                         if(box_on_high_i||box_on_low_k ){i=dim;j=  0;k= -1;for(j=0;j<dim;j++){int ijk=i+j*jStride+k*kStride;x[ijk]=s*x[ijk+normal];}}
+      s=1;normal=0;if(box_on_low_i ){normal+=      1;s*=-1;}if(box_on_high_k){normal-=kStride;s*=-1;}                                         if(box_on_low_i ||box_on_high_k){i= -1;j=  0;k=dim;for(j=0;j<dim;j++){int ijk=i+j*jStride+k*kStride;x[ijk]=s*x[ijk+normal];}}
+      s=1;normal=0;if(box_on_high_i){normal-=      1;s*=-1;}if(box_on_high_k){normal-=kStride;s*=-1;}                                         if(box_on_high_i||box_on_high_k){i=dim;j=  0;k=dim;for(j=0;j<dim;j++){int ijk=i+j*jStride+k*kStride;x[ijk]=s*x[ijk+normal];}}
+      s=1;normal=0;if(box_on_low_i ){normal+=      1;s*=-1;}if(box_on_low_j ){normal+=jStride;s*=-1;}                                         if(box_on_low_i ||box_on_low_j ){i= -1;j= -1;k=  0;for(k=0;k<dim;k++){int ijk=i+j*jStride+k*kStride;x[ijk]=s*x[ijk+normal];}}
+      s=1;normal=0;if(box_on_high_i){normal-=      1;s*=-1;}if(box_on_low_j ){normal+=jStride;s*=-1;}                                         if(box_on_high_i||box_on_low_j ){i=dim;j= -1;k=  0;for(k=0;k<dim;k++){int ijk=i+j*jStride+k*kStride;x[ijk]=s*x[ijk+normal];}}
+      s=1;normal=0;if(box_on_low_i ){normal+=      1;s*=-1;}if(box_on_high_j){normal-=jStride;s*=-1;}                                         if(box_on_low_i ||box_on_high_j){i= -1;j=dim;k=  0;for(k=0;k<dim;k++){int ijk=i+j*jStride+k*kStride;x[ijk]=s*x[ijk+normal];}}
+      s=1;normal=0;if(box_on_high_i){normal-=      1;s*=-1;}if(box_on_high_j){normal-=jStride;s*=-1;}                                         if(box_on_high_i||box_on_high_j){i=dim;j=dim;k=  0;for(k=0;k<dim;k++){int ijk=i+j*jStride+k*kStride;x[ijk]=s*x[ijk+normal];}}
+      
+      // calculate a normal vector for this corner                                                                                            // if corner is on a domain boundary, impose the boundary condition using the calculated normal
+      s=1;normal=0;if(box_on_low_i ){normal+=      1;s*=-1;}if(box_on_low_j ){normal+=jStride;s*=-1;}if(box_on_low_k ){normal+=kStride;s*=-1;}if(box_on_low_i || box_on_low_j || box_on_low_k ){i= -1;j= -1;k= -1;{int ijk=i+j*jStride+k*kStride;x[ijk]=s*x[ijk+normal];}}
+      s=1;normal=0;if(box_on_high_i){normal-=      1;s*=-1;}if(box_on_low_j ){normal+=jStride;s*=-1;}if(box_on_low_k ){normal+=kStride;s*=-1;}if(box_on_high_i|| box_on_low_j || box_on_low_k ){i=dim;j= -1;k= -1;{int ijk=i+j*jStride+k*kStride;x[ijk]=s*x[ijk+normal];}}
+      s=1;normal=0;if(box_on_low_i ){normal+=      1;s*=-1;}if(box_on_high_j){normal-=jStride;s*=-1;}if(box_on_low_k ){normal+=kStride;s*=-1;}if(box_on_low_i || box_on_high_j|| box_on_low_k ){i= -1;j=dim;k= -1;{int ijk=i+j*jStride+k*kStride;x[ijk]=s*x[ijk+normal];}}
+      s=1;normal=0;if(box_on_high_i){normal-=      1;s*=-1;}if(box_on_high_j){normal-=jStride;s*=-1;}if(box_on_low_k ){normal+=kStride;s*=-1;}if(box_on_high_i|| box_on_high_j|| box_on_low_k ){i=dim;j=dim;k= -1;{int ijk=i+j*jStride+k*kStride;x[ijk]=s*x[ijk+normal];}}
+      s=1;normal=0;if(box_on_low_i ){normal+=      1;s*=-1;}if(box_on_low_j ){normal+=jStride;s*=-1;}if(box_on_high_k){normal-=kStride;s*=-1;}if(box_on_low_i || box_on_low_j || box_on_high_k){i= -1;j= -1;k=dim;{int ijk=i+j*jStride+k*kStride;x[ijk]=s*x[ijk+normal];}}
+      s=1;normal=0;if(box_on_high_i){normal-=      1;s*=-1;}if(box_on_low_j ){normal+=jStride;s*=-1;}if(box_on_high_k){normal-=kStride;s*=-1;}if(box_on_high_i|| box_on_low_j || box_on_high_k){i=dim;j= -1;k=dim;{int ijk=i+j*jStride+k*kStride;x[ijk]=s*x[ijk+normal];}}
+      s=1;normal=0;if(box_on_low_i ){normal+=      1;s*=-1;}if(box_on_high_j){normal-=jStride;s*=-1;}if(box_on_high_k){normal-=kStride;s*=-1;}if(box_on_low_i || box_on_high_j|| box_on_high_k){i= -1;j=dim;k=dim;{int ijk=i+j*jStride+k*kStride;x[ijk]=s*x[ijk+normal];}}
+      s=1;normal=0;if(box_on_high_i){normal-=      1;s*=-1;}if(box_on_high_j){normal-=jStride;s*=-1;}if(box_on_high_k){normal-=kStride;s*=-1;}if(box_on_high_i|| box_on_high_j|| box_on_high_k){i=dim;j=dim;k=dim;{int ijk=i+j*jStride+k*kStride;x[ijk]=s*x[ijk+normal];}}
+
     }
   }
   level->cycles.boundary_conditions += (uint64_t)(CycleTime()-_timeStart);
